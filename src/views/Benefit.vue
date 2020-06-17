f9d116e07f1ccb83dd6b32ad72c86bd80f0642a8
BOMI <bnewbom@gamil.com>
2020-06-17 17:59:02 +0900

    Benefit view 기본 요소 작성

--- src/views/Benefit.vue
+++ src/views/Benefit.vue
@@ -1,9 +1,42 @@
 <template>
     <div>
         <h1>benefit</h1>
-        <header>헤더</header>
-        <section>메인 혜택</section>
-        <section>추천 혜택</section>
+
+        <!--summary-->
+        <section>
+            <p>6월 받은 혜택</p>
+            <span>0원</span>
+            <a>쿠폰</a>
+            <ul>
+                <li>할인&적립</li>
+                <li>업그레이드</li>
+            </ul>
+        </section>
+
+        <!--메인 혜택 리스트-->
+        <section>
+            <ul>
+                <li>
+                    <span>내문서함</span>
+                    <span>6.2~6.30</span>
+                    <a>미션 문서 도착!<br/>총 1천만원의 주인공은?!</a>
+                </li>
+            </ul>
+        </section>
+
+        <!--추천 혜택 더보기-->
+        <section>
+            <p>봄님을 위한 추천</p>
+            <a>모두 보기</a>
+
+            <ul>
+                <li>
+                    <span>하나은행</span>
+                    <strong>7천원+이모티콘</strong>
+                    <spna>수수료 무제한 면제계좌 개설 시</spna>
+                </li>
+            </ul>
+        </section>
     </div>
 </template>
 
