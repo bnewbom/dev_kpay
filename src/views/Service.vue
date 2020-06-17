aff861a884944c531bf9dda941cab04a72467550
BOMI <bnewbom@gamil.com>
2020-06-17 18:32:09 +0900

    Service view 기본 요소 작성

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,7 +1,58 @@
 <template>
   <div>
       <h1>service</h1>
-      <header>헤더</header>
+      <a>등급 영향 없이 무료로 신용조회하세요!
+        <span>오늘부터 마음 놓고 내 신용점수를 확인해보세요.</span>
+      </a>
+
+      <section>
+        <h2>설정</h2>
+        <p>비밀번호 결제수단 고객센터</p>
+      </section>
+
+      <section>
+        <h2>관리하기</h2>
+        <ul>
+          <li>통합내역</li>
+          <li>신용조회</li>
+          <li>영수증</li>
+          <li>청구서</li>
+          <li>인증</li>
+          <li>내문서함</li>
+          <li>내차관리</li>
+          <li>내 보험 관리</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>준비하기</h2>
+        <ul>
+          <li>자동차보험</li>
+          <li>전세보험</li>
+          <li>운전자보험</li>
+          <li>간편보험</li>
+          <li>운동보험</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>빌리기</h2>
+        <ul>
+          <li>대출비교</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>모으기</h2>
+        <ul>
+          <li>펀드</li>
+          <li>동전 모으기</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </section>
   </div>
 </template>
 
