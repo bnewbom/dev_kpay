856aa53ca84abcdaa6240e750915deb05b39427d
BOMI <bnewbom@gamil.com>
2020-06-23 11:11:11 +0900

    service menu slot

---
+++ src/components/ServiceMenus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+        <section>
+            <slot name="header">
+                <h2><!--Menu Catagory--></h2>
+            </slot>
+            <slot name="body">
+                <ul>
+                    <li><!--Menu List1--></li>
+                </ul>
+            </slot>
+        </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="service">
       <h1>service</h1>
-      <a class="ad">등급 영향 없이 무료로 신용조회하세요!
+      <a class="ad">
+        <p>등급 영향 없이 무료로 신용조회하세요!</p>
         <span>오늘부터 마음 놓고 내 신용점수를 확인해보세요.</span>
       </a>
 
@@ -9,89 +10,56 @@
         <h2>설정</h2>
         <span>비밀번호 결제수단 고객센터</span>
       </section>
-
-      <section>
-        <h2>관리하기</h2>
-        <ul>
-          <li>통합내역</li>
-          <li>신용조회</li>
-          <li>영수증</li>
-          <li>청구서</li>
-          <li>인증</li>
-          <li>내문서함</li>
-          <li>내차관리</li>
-          <li>내 보험 관리</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>준비하기</h2>
-        <ul>
-          <li>자동차보험</li>
-          <li>전세보험</li>
-          <li>운전자보험</li>
-          <li>간편보험</li>
-          <li>운동보험</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>빌리기</h2>
-        <ul>
-          <li>대출비교</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>모으기</h2>
-        <ul>
-          <li>펀드</li>
-          <li>동전 모으기</li>
-          <li>부동산 소액투자</li>
-          <li>신용 분산투자</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>쓰기</h2>
-        <ul>
-          <li>송금</li>
-          <li>결제</li>
-          <li>더치페이</li>
-          <li>페이카드</li>
-          <li>예약송금</li>
-          <li>멤버십</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>기타</h2>
-        <ul>
-          <li>환전</li>
-          <li>배송</li>
-          <li>이벤트</li>
-          <li>금융제휴</li>
-          <li>소호결제키트</li>
-        </ul>
-      </section>
-
-      <section>
-        <h2>사용처</h2>
-        <ul>
-          <li>매장결제</li>
-          <li>멤버십</li>
-          <li>청구서</li>
-          <li>온라인 결제</li>
-          <li>인증</li>
-        </ul>
-      </section>
+      <ServiceMenus v-for="(service, index) in serviceList" :key="index">
+        <template v-slot:header>
+          <h2>{{service.category}}</h2>
+        </template>
+        <template v-slot:body>
+          <ul>
+            <li v-for="menu in serviceList[index].title" :key="menu">{{menu}}</li>
+          </ul>
+        </template>
+      </ServiceMenus>
   </div>
 </template>
 
 <script>
+import ServiceMenus from '../components/ServiceMenus'
 export default {
     name: 'ServiceView',
-
+    components:{
+      ServiceMenus,
+    },
+    data: function(){
+      return{
+        serviceList:[
+          {
+            category:'관리하기',
+            title:['통합내역', '신용조회', '영수증', '청구서', '인증', '내문서함', '내차관리', '내 보험 관리']
+          },
+          {
+            category:'준비하기',
+            title:['자동차보험', '전세보험', '운전자보험', '간편보험', '운동보험']
+          },
+          {
+            category:'빌리기',
+            title:['대출비교']
+          },
+          {
+            category:'모으기',
+            title:['펀드', '동전 모으기', '부동산 소액투자', '', '신용 분산투자']
+          },
+          {
+            category:'쓰기',
+            title:['송금', '결제', '더치페이', '페이카드', '예약송금', '멤버십']
+          },
+          {
+            category:'',
+            title:['환전', '배송', '이벤트', '금융제휴', '소호결제키트']
+          },
+        ]
+      }
+    }
 }
 </script>
 
